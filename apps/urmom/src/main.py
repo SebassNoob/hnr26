37c56442ea299a7c0f0bf3e55f846c86d2c88af0
TheLegendaryZHR <zhuohaoran2006@gmail.com>
2026-01-17 14:35:39 +0800

    added functionality to use json as argument

--- apps/urmom/src/main.py
+++ apps/urmom/src/main.py
@@ -1,7 +1,21 @@
 from features import food
 from features import shutdown
+import sys
+import json
 
 def main():
+    if len(sys.argv) != 2:
+        print("Error: expected only one json string as argument")
+        return
+    try:
+        json_args = json.loads(sys.argv[1])
+    except json.JSONDecodeError:
+        print("Error: argument is not valid json")
+        return
+    lights_out_time = json_args["lightsOut"]
+    blacklisted_processes = json_args["blacklistedProcesses"]
+    nag = json_args["nag"]
+    slipper_enabled = json_args["slipperEnabled"]
     shutdown.main()
     print("Hello from urmom!")
 
