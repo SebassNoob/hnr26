910126b83502f121b5786f7318cef44e93ff35a0
TheLegendaryZHR <zhuohaoran2006@gmail.com>
2026-01-17 20:18:06 +0800

    activewindow

--- apps/urmom/src/features/windows_api/activewindow.py
+++ apps/urmom/src/features/windows_api/activewindow.py
@@ -2,20 +2,19 @@ import win32gui
 import win32process
 import psutil
 
-
 def get_active_process_info():
     hwnd = win32gui.GetForegroundWindow()
-
+    
     # Get the Process ID (PID) from the window handle
     _, pid = win32process.GetWindowThreadProcessId(hwnd)
-
+    
     try:
         # Use psutil to get the process name from the PID
         process = psutil.Process(pid)
         return {
             "title": win32gui.GetWindowText(hwnd),
             "name": process.name(),
-            "pid": pid,
+            "pid": pid
         }
     except psutil.NoSuchProcess:
         return None
@@ -23,7 +22,6 @@ def get_active_process_info():
 
 def main():
     pass
-
 
 if __name__ == "__main__":
     main()
